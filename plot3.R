5d2ea313d9d8b0ab2d9946b34c3ea500758b919d
rbabu <babu.rakesh.n@gmail.com>
2014-05-10 11:56:04 -0400

    finished plot 3

--- plot3.R
+++ plot3.R
@@ -1,4 +1,4 @@
-# Create plot1.png
+# Create plot3.png
 
 #Read in all raw data
 rawData <- read.table("household_power_consumption.txt", header=F, sep=";", colClasses = "character")
@@ -10,10 +10,19 @@ myData <- cbind(strptime(paste(myData[,1], myData[,2]), format="%d/%m/%Y %H:%M:%
 names(myData) <- c("Date/Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
 
 # Open "PNG" device
-png(filename="plot1.png", width=480, height=480)
+png(filename="plot3.png", width=480, height=480)
 # Prepare the plot, but do not add any points
-plot(myData$Date, as.numeric(myData$Global_active_power), pch=NA, main="", ylab = "Global Active Power (kilowatts)", xlab = "")
-# Connect the dots
-lines(myData$Date, as.numeric(myData$Global_active_power))
+plot(myData$Date, as.numeric(myData$Sub_metering_1), pch=NA, main="", ylab = "Energy Sub Metering", xlab = "")
+# Connect the dots: black is station 1, red is 2, blue is 3
+lines(myData$Date, as.numeric(myData$Sub_metering_1), col="black")
+lines(myData$Date, as.numeric(myData$Sub_metering_2), col="red")
+lines(myData$Date, as.numeric(myData$Sub_metering_3), col="blue")
+# Add a legent
+legend("topright",
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty=c(1,1,1),
+       lwd=c(1,1,1),
+       col=c("black", "red", "blue"))
+       
 # Close the "PNG" device
 dev.off()
